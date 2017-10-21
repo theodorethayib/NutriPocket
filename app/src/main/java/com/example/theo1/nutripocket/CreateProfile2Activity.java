a13a8548a628685ba871d02dd10db2e3a1c297bc
Theodore Thayib <tpthayib@iastate.edu>
2017-10-20 22:26:09 -0500

    Blank Create Profile/Main Activity

--- app/src/main/java/com/example/theo1/nutripocket/CreateProfile2Activity.java
+++ app/src/main/java/com/example/theo1/nutripocket/CreateProfile2Activity.java
@@ -1,13 +1,41 @@
 package com.example.theo1.nutripocket;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class CreateProfile2Activity extends AppCompatActivity {
+
+    Button CP2Info;
+    Button buttonContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_profile2);
+        getSupportActionBar().hide();
+
+        CP2Info = (Button) findViewById(R.id.CP2Info);
+
+        CP2Info.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(CreateProfile2Activity.this, activityLevelDescriptionsActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        buttonContinue = (Button) findViewById(R.id.CP2Continue);
+
+        buttonContinue.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(CreateProfile2Activity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
