6e589914fd1243cfcaefaad326ad06c3f28d2ea1
Theodore Thayib <tpthayib@iastate.edu>
2017-10-20 22:08:10 -0500

    Blank Create Profile/Main Activity

--- app/src/main/java/com/example/theo1/nutripocket/CreateProfileActivity.java
+++ app/src/main/java/com/example/theo1/nutripocket/CreateProfileActivity.java
@@ -1,13 +1,25 @@
 package com.example.theo1.nutripocket;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateProfileActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_profile);
+        button = (Button) findViewById(R.id.CP1Button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(CreateProfileActivity.this, CreateProfile2Activity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
