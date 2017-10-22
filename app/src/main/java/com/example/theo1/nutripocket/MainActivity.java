4bd6f97af8efcf80470ac2c9aaaba52001dc5ebe
Theodore Thayib <tpthayib@iastate.edu>
2017-10-21 22:31:00 -0500

    ProgressBar

--- app/src/main/java/com/example/theo1/nutripocket/MainActivity.java
+++ app/src/main/java/com/example/theo1/nutripocket/MainActivity.java
@@ -6,11 +6,12 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
-
+import android.widget.Button;
 import static android.util.Log.d;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     TextView prefTitle;
     TextView calorieGoal;
     ProgressBar calorieProgressBar;
+    int calorieCount = 0;
+    Button breakdownButton;
+    TextView currentCalories;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,21 @@ public class MainActivity extends AppCompatActivity {
         prefTitle = (TextView) findViewById(R.id.prefRectTitle);
         calorieGoal = (TextView) findViewById(R.id.calorieGoal);
         calorieProgressBar = (ProgressBar) findViewById(R.id.calorieProgressBar);
+        breakdownButton = (Button) findViewById(R.id.caloricBreakdownButton);
+        currentCalories = (TextView) findViewById(R.id.currentCalories);
+
+
+
+
+        breakdownButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                calorieCount += 25;
+                calorieProgressBar.setProgress(calorieCount);
+                currentCalories.setText("You are currently at " + calorieCount + "Calories!");
+            }
+            });
+
+
 
 
         SharedPreferences sharedPrefs = getSharedPreferences("userInfo", MODE_PRIVATE);
@@ -48,5 +67,6 @@ public class MainActivity extends AppCompatActivity {
         calculateCaloricGoal calGoal = new calculateCaloricGoal(userInfo);
         int goalAmount = calGoal.calculate();
         calorieGoal.setText("Daily Calorie Goal: " + String.valueOf(goalAmount));
+        calorieProgressBar.setMax(goalAmount);
    }
 }
