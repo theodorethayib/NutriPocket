a9ad43705c4bbee3dc3965a5b6a51cc1e0456a41
Theodore Thayib <tpthayib@iastate.edu>
2017-10-21 23:27:50 -0500

    ProgressBar

--- app/src/main/java/com/example/theo1/nutripocket/Preferences.java
+++ app/src/main/java/com/example/theo1/nutripocket/Preferences.java
@@ -9,5 +9,6 @@ public class Preferences extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
+        getSupportActionBar().hide();
     }
 }
