a21ed4eff38e97af34c32d81173d25067ea7e0f7
Bachtri <nbachtri@gmail.com>
2017-10-21 22:27:10 -0500

    visual CP1

---
+++ app/src/main/java/com/example/theo1/nutripocket/BodyTypeDescription.java
@@ -0,0 +1,13 @@
+package com.example.theo1.nutripocket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BodyTypeDescription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_body_type_description);
+    }
+}
